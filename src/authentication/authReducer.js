242ab51ce4f596aa0c35a496d52b9d0067c39383
xhepan <102022513+xhepan@users.noreply.github.com>
2022-09-01 20:28:05 +0200

    Add files via upload

---
+++ src/authentication/authReducer.js
@@ -0,0 +1,41 @@
+const initState = {
+    authError: null
+} ;
+
+const authReducer = (state = initState, action) => {
+    switch(action.type){
+        case 'LOGIN_FAILED':
+            console.log('failed');
+            return{
+                ...state,
+                authError: 'login failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('success');
+            return{
+                ...state,
+                authError: null
+            }
+        case 'EMAIL_SENT':
+            console.log('email sent');
+            console.log(state);
+            return{
+                ...state,
+                authError: null
+            }
+        case 'WRONG_EMAIL':
+            console.log('failed');
+            return{
+                ...state,
+                authError: 'rest failed'
+            }
+        case 'SIGNOUT_SUCCESS':
+            console.log('signed out !')
+            return state;
+        default:
+            return state;
+
+    }
+}
+ 
+export default authReducer;
---
+++ src/authentication/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+import postReducer from "./postReducer";
+import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase";
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    post: postReducer,
+    fstore: firestoreReducer,
+    firebase: firebaseReducer       //includes authentication
+});
+
+export default rootReducer;
