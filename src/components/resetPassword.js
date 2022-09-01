6e7c2cd96a75241fc047669d5e68cd4dbfc41147
xhepan <102022513+xhepan@users.noreply.github.com>
2022-09-01 20:20:07 +0200

    Add files via upload

---
+++ src/components/resetPassword.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { forgotPassword } from "../authActions";
+
+class resetPassword extends Component{
+    state = {
+        //stuNum: '',
+        email: ''
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        //console.log(this.state);
+        this.props.forgotPassword(this.state);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    // handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     //console.log(this.state);
+    //     this.props.signin(this.state);
+    // }
+
+    render(){
+        //const { authError } = this.props;
+        return(
+            <div className="passReset">
+                <h2>Password Reset</h2>
+                <form onSubmit={this.handleSubmit}>          
+                {/* <label htmlFor="stuNum">studdent number:</label> */}
+                    <label htmlFor="email">email:</label>
+                    <input type="email" id="email" required onChange={this.handleChange}></input>
+                    <button>enter</button> 
+                    {/* <a href='/signin' onClick={this.handleForgot}>Forgot Password</a> */}
+                    {/* <label>
+                    { authError ? <p>{ authError }</p> : null}    
+                    </label>               */}
+            </form>
+
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return{
+        authError: state.auth.authError
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{ 
+        //signin: (creds) => dispatch(signin(creds)),
+        forgotPassword: (creds) => dispatch(forgotPassword(creds))
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(resetPassword);
