b5d4273d36ee80203d61dde9a65f6deff29bf43a
TRJMajika <101942468+TRJMajika@users.noreply.github.com>
2022-09-10 17:00:36 +0200

    Retrieve all Posts form

---
+++ src/components/AllPostsT.js
@@ -0,0 +1,55 @@
+///////Thabelo:
+
+import React, { useState, useEffect } from 'react'
+import firebase from '../firebase/index' 
+import {Container} from '@material-ui/core'
+
+
+function AllPostsT() {
+    const [posts, setPosts] = useState([]) ;
+
+    const ref = firebase.firestore().collection("UserPosts");
+
+    function AllPosts(){
+        ref.onSnapshot((querySnapshot) => { 
+            const items = [] ;
+            querySnapshot.forEach((doc) => {items.push(doc.data()) ;
+            }) ;
+            setPosts(items) ;
+        }) ;
+    }
+
+    useEffect(() => { AllPosts() }, []) ;
+
+    return (
+        <div>
+             <h2>ALL POSTS</h2>
+             
+            {posts.map((post) => (
+                <div key= {post.u_id}> 
+                <Container maxWidth="lg" style={{backgroundColor:"whitesmoke"}}>
+                    <h5>Caption :  {post.u_caption}</h5>
+                    <h5>Question - </h5>
+                    <p> {post.u_question}</p>
+                    <button type="details" variant="contained" color="primary">View in detail</button>
+                </Container>
+                </div>
+            ))}
+       
+  
+          
+        </div>
+      
+     
+           
+
+        
+        
+  )
+
+}
+
+export default AllPostsT ;
+
+
+///////Thabelo:
