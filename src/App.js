53f2a630d7ae6f747da85dacc8fdd042b5335064
TRJMajika <101942468+TRJMajika@users.noreply.github.com>
2022-09-12 02:53:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Dashboard from "./components/dashboard";
 import { useUserContext } from "./context/userContext";
 import "./index.css";
 
-
+///////Thabelo:
+import PostsT from "./components/PostsT";
+import AllPostsT  from "./components/AllPostsT";
+///////Thabelo:
 
 function App() {
   const { user, loading, error } = useUserContext();
@@ -12,6 +15,17 @@ function App() {
     <div className="App">
       {error && <p className="error">{error}</p>}
       {loading ? <h2>Loading...</h2> : <> {user ? <Dashboard /> : <Auth />} </>}
+       
+       {/* Thabelo: */}
+      <Router>
+      <Routes>
+        <Route path="/posts" element={<PostsT/>}/>
+        <Route path="/allposts" element={<AllPostsT/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+      </Router>
+    {/* Thabelo */}
+       
       
     </div>
     
