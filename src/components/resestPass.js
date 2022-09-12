e52bba966829f49869a83269cb7bd95bab49917f
xhepan <102022513+xhepan@users.noreply.github.com>
2022-09-12 02:53:17 +0200

    Add files via upload

---
+++ src/components/resestPass.js
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+import { useUserContext } from "../context/userContext";
+
+const Reset = () => {
+    const emailRef = useRef();
+    const { forgotpassword } = useUserContext();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const email = emailRef.current.value;
+        if (email) forgotpassword(email);
+    };
+    return (
+        <div className="form">
+          <h2> reset password </h2>
+          <form onSubmit={onSubmit}>
+            <input placeholder="Email" type="email" required ref={emailRef} />
+            <button type="submit">send</button>
+            
+          </form>
+        </div>
+    );
+};
+
+export default Reset;
